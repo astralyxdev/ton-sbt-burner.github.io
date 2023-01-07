e887b7f408f140d6cc32bd2a0bdb637e6b1709fe
Daniil <84060706+danya7423@users.noreply.github.com>
2023-01-07 23:45:25 +0600

    ы

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let transfer_link = ''
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 async function checkIsSBT(address) {
-    await timer(1000)
+    await timer(10000)
     let source = await fetch(`https://api.ton.cat/v2/contracts/address/${address}/source`)
     source = await source.json()
 
